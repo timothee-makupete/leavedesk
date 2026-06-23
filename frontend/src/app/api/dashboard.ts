3d0e6ee5765e83e1286d784cdb69ca3c1cffb970
Timothy phiri Makupete <timotheemakupete@gmail.com>
2026-06-23 11:02:32 +0200

    downgrade

--- frontend/src/app/api/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "./client";
-import type { DashboardStats } from "./types";
-
-export const dashboardApi = {
-  async stats(): Promise<DashboardStats> {
-    const { data } = await api.get<DashboardStats>("/admin/dashboard/");
-    return data;
-  },
-};
--- frontend/src/routes/$.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AppMount } from "../app/AppMount";
-
-export const Route = createFileRoute("/$")({
-  component: AppMount,
-});
